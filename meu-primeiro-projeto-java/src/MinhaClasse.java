bf668ee22416d1c1d66bf3fe34caaf447b7a550a
Lucas <lucas.hen.lps@gmail.com>
2024-04-24 10:01:13 -0300

    anatomia das classes 1

---
+++ meu-primeiro-projeto-java/src/MinhaClasse.java
@@ -0,0 +1,7 @@
+public class MinhaClasse { //deve conter o mesmo nome do arquivo
+    public static void main(String[] args){
+        System.out.println("Olá turma! Sejam bem-vindos."); //Classe System que tem métodos out e println
+        //Arquivos .java são classes, ou seja, por convenção, a inicial de cada palavra deve ser letra maiúscula!
+
+    }
+}
