02c7629a0fc257d264dedde5791ed978999844bc
Lucas <lucas.hen.lps@gmail.com>
2024-05-22 10:08:47 -0300

    finalizando desafio

--- bootcamp-projeto/src/Main.java
+++ bootcamp-projeto/src/Main.java
@@ -1,5 +1,3 @@
-import java.time.LocalDate;
-
 public class Main {
     public static void main(String[] args) {
         Curso curso = new Curso();
@@ -12,11 +10,25 @@ public class Main {
 
         Mentoria mentoria = new Mentoria(); 
 
-        mentoria.setTitulo("Mentoria 1");
-        mentoria.setDescricao("A primeira mentoria do site");
-        mentoria.setData(LocalDate.now());
-        
-        System.out.println(mentoria);
+        mentoria.setTitulo("Mentoria Java Developer");
+        mentoria.setDescricao("Descrição Mentoria Java Developer");
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev dev = new Dev();
+        dev.setNome("Lucas");
+        dev.inscrever(bootcamp);
+
+        Dev paulo = new Dev();
+        paulo.setNome("Paulo");
+        paulo.inscrever(bootcamp);
+
+        System.out.println("Conteúdos inscritos " + dev.getConteudosInscritos());
+        dev.progredir();
+        System.out.println("Conteúdos concluídos" + dev.getConteudosConcluidos());
     }
 }
